cd8a541d1d9f4b875e5dd68b62b1e67266695271
Adnan Furkan Aktemur <60827468+AdnanFurkanAKTEMUR@users.noreply.github.com>
2023-02-22 11:21:37 +0300

    Update mongo.js

--- db/mongo.js
+++ db/mongo.js
@@ -3,7 +3,6 @@ let client
 async function mongo(){
   //atlas url
   //const uri = "mongodb+srv://@rafyonetimi.uiemr8z.mongodb.net/counter"
-  //const uri = "mongodb://localhost:27017/counter"
   const uri = "mongodb://test-user:test-pass@localhost:27011/?replicaSet=replicaset-example&serverSelectionTimeoutMS=2000&authSource=admin&appName=mongosh+1.6.1/counter"
   client = new MongoClient(uri)
   try{
